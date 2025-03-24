fd54fdcbb3c4dd38cc17f91ecf97e06e239c4d10
Imposter <ara404.github@gmail.com>
2025-03-24 13:27:59 +0530

    Add files via upload

--- a.php
+++ a.php
@@ -1,40 +1,44 @@
 <?php
-// Ensure the MongoDB extension is loaded
-if (!extension_loaded('mongodb')) {
-    die('MongoDB extension is not loaded');
-}
-// require "..vendor/autoload.php";
-require(__DIR__."/phpmongodb/vendor/autoload.php");
-// Create a MongoDB client
-$client = new MongoDB\Client("mongodb://localhost:27017"); // Replace with your connection string
-echo "12";
-// // Select a database
-$database = $client->selectDatabase('Project');
+// // Ensure the MongoDB extension is loaded
+// if (!extension_loaded('mongodb')) {
+//     die('MongoDB extension is not loaded');
+// }
+// // require "..vendor/autoload.php";
+// require(__DIR__."/phpmongodb/vendor/autoload.php");
+// // Create a MongoDB client
+// $client = new MongoDB\Client("mongodb://localhost:27017"); // Replace with your connection string
+// echo "12";
+// // // Select a database
+// $database = $client->selectDatabase('Project');
 
-// Select a collection
-// $collection = $database->selectCollection('Employee');
-$collection = $database->selectCollection('vote');
+// // Select a collection
+// // $collection = $database->selectCollection('Employee');
+// $collection = $database->selectCollection('vote');
 
-// // Example: Insert a document
-// $insertResult = $collection->insertOne([
-//     'name' => 'John Doe',
-//     'email' => 'john@example.com',
-//     "zz"=>[1,2,3,4],
-// ]);
+// // // Example: Insert a document
+// // $insertResult = $collection->insertOne([
+// //     'name' => 'John Doe',
+// //     'email' => 'john@example.com',
+// //     "zz"=>[1,2,3,4],
+// // ]);
 
-// echo "Inserted document with ID: " . $insertResult->getInsertedId() . "\n";
+// // echo "Inserted document with ID: " . $insertResult->getInsertedId() . "\n";
 
-// // Example: Find a document
-// $document = $collection->findOne(['name' => 'John Doe']);
-$document = $collection->find();
-// $document = $collection->find(['type' => 'core']);
+// // // Example: Find a document
+// // $document = $collection->findOne(['name' => 'John Doe']);
+// $document = $collection->find();
+// // $document = $collection->find(['type' => 'core']);
 
-foreach($document as $r){
+// foreach($document as $r){
 
-    print_r($r["zz"]);
-    // print_r($document->zz);
-}
-// print_r($document);
+//     print_r($r["zz"]);
+//     // print_r($document->zz);
+// }
+// // print_r($document);
+
+$k="20";
+
+
 ?>
 
 
