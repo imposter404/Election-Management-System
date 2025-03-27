5bc8c1dca180fa8fb5e81b230316bb01d7074ee7
Imposter <ara404.github@gmail.com>
2025-03-27 20:08:32 +0530

    Add files via upload

--- Register.php
+++ Register.php
@@ -8,80 +8,29 @@ require(__DIR__."/phpmongodb/vendor/autoload.php");
 
 $client = new MongoDB\Client("mongodb://localhost:27017"); // Replace with your connection string
 
-$database = $client->selectDatabase('Project');;
-$collection = $database->selectCollection('vote');
-
-
-class obj{} //object define
-$a=new obj();
-
-$a->g=10;
-$a->h=20;
-
-$name=new obj();
-$name->first_Name='John';
-$name->last_Name ='Doe';
-
-$user=new obj();
-$user->user_id="a123";
-$user->user_id_hash="001";
-$user->password='a123';
-$user->password_hash='200';
+$database = $client->selectDatabase('Project');
+$collection = $database->selectCollection('user');
 
 
 
-// Example: Insert a document
-// $insertResult = $collection->insertOne([
-//     'voter_id'=>'ABCD1234',
-//     'name'=>$name,
-//     'email' => 'john@example.com',
-//     'phone_no' => '123456789',
-//     'gender' => 'Male',
-//     'date_of_birth' => '21/5/2000',
-//     'user'=>$user,
-//     'SessionToken'=>''
-// ]);
+$q=$_REQUEST['q'];
+$r=json_decode($q);
 
-// echo "Inserted document with ID: " . $insertResult->getInsertedId() . "\n";
-
-// $ao=session_create_id();
-
-// echo $ao
-
-
-
-$document = $collection->findOne(['voter_id'=>"ABCD123"]);
-
-
-$voter_id="V".date("Ymd").rand(0,1000);
-print_r($voter_id);
-
-
-// $document = $collection->findOne(['user.user_id_hash'=>"001",'user.password_hash'=>"200"]);
-// // $document = $collection->find([' type' => 'core']);
-
-if($document)
+$document = $collection->findOne(['user_id'=>$r->user_id]);
+if(empty($document))
 {
-    echo "found";
+    $insertResult = $collection->insertOne([
+        'user'=>[
+            'user_id'=>$r->user_id,
+            'password'=>$r->password,
+            'user_id_hash'=>hash('sha256',$r->user_id),
+            'password_hash'=>hash('sha256',$r->password)
+        ],
+    ]);
+    echo 'valid';
 }
 else{
-    echo "  nooot found";
+    echo 'invalid';
 }
 
-
-
-
-// foreach($document as $r){
-//     print_r($r["name"]);
-// }
-// echo $document->name->g;
-
-print_r($document->name);
-print_r($document->email);
-
-
 ?>
-
-
-
-
--- login Copy.php
+++ login Copy.php
@@ -12,38 +12,11 @@ $database = $client->selectDatabase('Project');;
 $collection = $database->selectCollection('user');
 
 
-session_start();
 
 $q=$_REQUEST['q'];
 $r=json_decode($q);
-// $fn=$r->user_id;
-// $_SESSION["Token"]="12";
-
-
-if($r->SessionToken !="")
-{
-    if($r->SessionToken==$_SESSION["Token"])
-    {
-        echo "valid";
-    }
-    else{
-        echo "invalid";
-    }
-}
-else{
-    // echo "no";
-    z();
-}
-// echo $r;
-// print_r(json_encode($q));
-// print_r($r->SessionToken);
-
-
-
-
-function z(){
-global $r;
-global $collection;
+// $fn=$r->do;
+$fn=$r->user_id;
 
 $user_hash=hash('sha256',$r->user_id);
 $password_hash=hash('sha256',$r->password);
@@ -57,7 +30,7 @@ $a=new obj();
 $g=125;
 
 $document = $collection->findOne(['user_hash'=>$user_hash]);
-// $filter = ['_id' => new MongoDB\BSON\ObjectId($document->_id)];
+$filter = ['_id' => new MongoDB\BSON\ObjectId($document->_id)];
 if($document)
 {
     $a->user_id='true';
@@ -65,9 +38,14 @@ if($document)
     if($document)
     {
         $a->password='true';
-        $a->SessionToken=session_create_id();
-        $_SESSION["Token"]=$a->SessionToken;
-        $_SESSION["ID"]=$document->_id;
+        if($document->SessionToken == "")
+        {
+            $a->SessionToken=session_create_id();
+            session_start();
+            $_SESSION["Token"]=$a->SessionToken;
+            // $update = ['$set' => ['SessionToken' =>$a->SessionToken]];
+            // $result = $collection->updateOne($filter, $update);
+        }
     }
     else{
         $a->password='false';
@@ -76,8 +54,6 @@ if($document)
 else{
     $a->user_id='false';
 }
-print_r(json_encode($a));
-}
 
-// z();
+print_r(json_encode($a));
 ?>
